9069984ed4295f32f3620e49bf3e023b1ba2c881
upekshaip <upekshaip@gmail.com>
2024-08-30 01:21:38 +0530

    added external packeges

---
+++ api/Models/Stock.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models;
+
+public class Stock
+{
+    public int Id { get; set; }
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18, 2)")]
+
+    public decimal Purchase { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
+
+    public decimal LastDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+
+}
